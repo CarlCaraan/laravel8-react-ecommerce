1ca70624ce45ea6584b377e9f28ea8d65530e1c3
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-05-25 16:39:54 +0800

    5/25/22

--- frontend-react/src/pages/UserLoginPage.jsx
+++ frontend-react/src/pages/UserLoginPage.jsx
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from "react";
 import FooterDesktop from "../components/common/FooterDesktop";
 import NavMenuDesktop from "../components/common/NavMenuDesktop";
 import UserLogin from "../components/common/UserLogin";
-import Campaign from "../components/home/Campaign";
 
 class HomePage extends Component {
   render() {
