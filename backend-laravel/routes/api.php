1b22d6ba40e4ec65e39d1f0e94a2e72b57a34961
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-06-10 21:56:06 +0800

    6/10/22

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -3,17 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Admin\VisitorController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
