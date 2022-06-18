2cfdae9de94e53eb7e9e12e93b406100ec6bab7c
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-06-18 22:24:55 +0800

    6/18/22

--- frontend-react/src/components/home/Categories.jsx
+++ frontend-react/src/components/home/Categories.jsx
@@ -1,398 +1,69 @@
 import React, { Component, Fragment } from "react";
 import { Container, Col, Row, Card } from "react-bootstrap";
 import classes from "./Categories.module.css";
+import axios from "axios";
+import AppURL from "../../api/AppURL";
 
 class Categories extends Component {
+  constructor() {
+    super();
+    this.state = {
+      MenuData: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(AppURL.AllCategoryDetails)
+      .then((response) => {
+        this.setState({
+          MenuData: response.data,
+        });
+      })
+      .catch((error) => {});
+  }
+
   render() {
+    const CategoryList = this.state.MenuData;
+    const MyView = CategoryList.map((CategoryList, i) => {
+      return (
+        <Col
+          xl={2}
+          lg={3}
+          md={4}
+          sm={6}
+          xs={12}
+          className="p-0"
+          key={i.toString()}
+        >
+          <Card
+            className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
+          >
+            <Container>
+              <Card.Img
+                className="img-fluid"
+                variant="top"
+                src={CategoryList.category_image}
+                alt="category-1"
+              />
+            </Container>
+            <Card.Body className="text-center px-0 pt-2 pb-0">
+              <span className="card-product-title">
+                {CategoryList.category_name}
+              </span>
+            </Card.Body>
+          </Card>
+        </Col>
+      );
+    });
+
     return (
       <Fragment>
         <Container>
           <span className="section-header">Categories</span>
           <Row className="m-0">
-            <Col className="p-0" xl={6} lg={6} md={12} sm={12} xs={12}>
-              <Row className="p-3">
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={2} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={3} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={4} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={5} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={6} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={7} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={8} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-
-            <Col className="p-0" xl={6} lg={6} md={12} sm={12} xs={12}>
-              <Row className="p-3">
-                <Col className="p-0" key={9} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={10}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={11}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={12}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-end-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={13}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={14}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={15}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col
-                  className="p-0"
-                  key={16}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={6}
-                  xs={6}
-                >
-                  <Card
-                    className={`${classes["card-categories"]} py-3 border border-top-0 border-start-0 border-bottom-0 border-end-0`}
-                  >
-                    <Container>
-                      <Card.Img
-                        variant="top"
-                        src="https://lzd-img-global.slatic.net/g/p/78a433741f99615fb2013dad995c29ff.jpg_80x80q80.jpg_.webp"
-                        alt="category-1"
-                        width="80px"
-                        height="80px"
-                      />
-                    </Container>
-                    <Card.Body className="text-center px-0 pt-2 pb-0">
-                      <span className="card-product-title">
-                        Home Office Chairs
-                      </span>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
+            <Col className="p-0" xl={12} lg={12} md={12} sm={12} xs={12}>
+              <Row className="p-3">{MyView}</Row>
             </Col>
           </Row>
         </Container>
--- frontend-react/src/components/home/MegaMenuMobile.jsx
+++ frontend-react/src/components/home/MegaMenuMobile.jsx
@@ -2,11 +2,56 @@ import React, { Fragment, useState } from "react";
 import { Offcanvas, Accordion } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import classes from "./MegaMenuMobile.module.css";
+import { useEffect } from "react";
+import axios from "axios";
+import AppURL from "../../api/AppURL";
 
 function MegaMenuMobile({ name, ...props }) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const toggleShow = () => setShow((s) => !s);
+
+  const [MenuData, setMenuData] = useState([]);
+  const [loaderDiv, setLoaderDiv] = useState("");
+  const [mainDiv, setMainDiv] = useState("d-none");
+
+  useEffect(() => {
+    axios
+      .get(AppURL.AllCategoryDetails)
+      .then((response) => {
+        setMenuData(response.data);
+        setLoaderDiv("d-none");
+        setMainDiv("");
+      })
+      .catch((error) => {});
+  }, []);
+  const CategoryLists = MenuData;
+  const MyView = CategoryLists.map((CategoryList, i) => (
+    <Accordion key={i.toString()} className={mainDiv}>
+      <Accordion.Item className={`${classes["off-canvas-body"]}`} eventKey="0">
+        <Accordion.Header>
+          <span className={`${classes["accordion-button"]}`}>
+            {CategoryList.category_name}
+          </span>
+        </Accordion.Header>
+        <Accordion.Body className="px-0 pb-0">
+          <div className={`${classes["megadrop"]}`}>
+            <ul>
+              <li>
+                <Link to="#">Mobiles</Link>
+              </li>
+              <li>
+                <Link to="#">Desktop</Link>
+              </li>
+              <li>
+                <Link to="#">Laptop</Link>
+              </li>
+            </ul>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  ));
 
   return (
     <Fragment>
@@ -31,62 +76,53 @@ function MegaMenuMobile({ name, ...props }) {
           <Offcanvas.Title>Categories</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          {/* Start Accordion */}
-          <Accordion>
-            <Accordion.Item
-              className={`${classes["off-canvas-body"]}`}
-              eventKey="0"
-            >
-              <Accordion.Header>
-                <span className={`${classes["accordion-button"]}`}>
-                  Electronic Device
-                </span>
-              </Accordion.Header>
-              <Accordion.Body className="px-0 pb-0">
-                <div className={`${classes["megadrop"]}`}>
-                  <ul>
-                    <li>
-                      <Link to="#">Mobiles</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Desktop</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Laptop</Link>
-                    </li>
-                  </ul>
+          {/* ========= Start Accordion ========= */}
+          {/* Start Skeletal Loading Div */}
+          <div className={loaderDiv}>
+            <div className="ph-item">
+              <div className="ph-col-12">
+                <div className="ph-row">
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
+                  <div className="ph-col-12"></div>
                 </div>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-          <Accordion>
-            <Accordion.Item
-              className={`${classes["off-canvas-body"]}`}
-              eventKey="0"
-            >
-              <Accordion.Header>
-                <span className={`${classes["accordion-button"]}`}>
-                  Electronic Accessories
-                </span>
-              </Accordion.Header>
-              <Accordion.Body className="px-0 pb-0">
-                <div className={`${classes["megadrop"]}`}>
-                  <ul>
-                    <li>
-                      <Link to="#">Mobile Accessories</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Audio</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Computer Accessories</Link>
-                    </li>
-                  </ul>
-                </div>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-          {/* End Accordion */}
+              </div>
+            </div>
+          </div>
+          {/* End Skeletal Loading Div */}
+          {MyView}
+          {/* ========= End Accordion ========= */}
         </Offcanvas.Body>
       </Offcanvas>
     </Fragment>
