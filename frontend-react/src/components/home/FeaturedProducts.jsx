10575101ef5ab4d38ad8c42d5dd78a596b72f5db
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-06-25 21:37:33 +0800

    6/25/22

--- frontend-react/src/components/home/FeaturedProducts.jsx
+++ frontend-react/src/components/home/FeaturedProducts.jsx
@@ -26,25 +26,50 @@ class FeaturedProducts extends Component {
   render() {
     const FeaturedList = this.state.ProductData;
     const MyView = FeaturedList.map((FeaturedList, i) => {
-      return (
-        <Col key={i.toString()} xl={2} lg={2} md={2} sm={4} xs={6}>
-          <Card className="card-product">
-            <Card.Img
-              variant="top"
-              src="https://lzd-img-global.slatic.net/g/p/35f1bfaa1d8c001450cc6634f455ff92.jpg_200x200q80.jpg_.webp"
-              alt="3b-button-3"
-            />
-            <Card.Body>
-              <span className="card-product-title">{FeaturedList.title}</span>
-              <br />
-              <span className="card-product-price">₱2.00</span>
-              <br />
-              <strike className="card-product-discount">₱799.00</strike>
-              <span className="card-product-discount-percent"> -71%</span>
-            </Card.Body>
-          </Card>
-        </Col>
-      );
+      if (FeaturedList.special_price === "na") {
+        return (
+          <Col key={i.toString()} xl={2} lg={2} md={2} sm={4} xs={6}>
+            <Card className="card-product">
+              <Card.Img
+                variant="top"
+                src={FeaturedList.image}
+                alt="3b-button-3"
+              />
+              <Card.Body>
+                <span className="card-product-title">{FeaturedList.title}</span>
+                <br />
+                <span className="card-product-price">
+                  ₱{FeaturedList.price}
+                </span>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      } else {
+        return (
+          <Col key={i.toString()} xl={2} lg={2} md={2} sm={4} xs={6}>
+            <Card className="card-product">
+              <Card.Img
+                variant="top"
+                src={FeaturedList.image}
+                alt="3b-button-3"
+              />
+              <Card.Body>
+                <span className="card-product-title">{FeaturedList.title}</span>
+                <br />
+                <span className="card-product-price">
+                  ₱{FeaturedList.special_price}
+                </span>
+                <br />
+                <strike className="card-product-discount">
+                  ₱{FeaturedList.price}
+                </strike>
+                <span className="card-product-discount-percent"> -71%</span>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
     });
 
     return (
