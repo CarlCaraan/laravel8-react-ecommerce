754bf22b49b4da0f46a2719705c43bfdb7e71626
Carl Caraan <bannedefused@gmail.com>
2022-07-11 11:13:44 +0800

    7/11/22

--- frontend-react/src/validation/Validation.js
+++ frontend-react/src/validation/Validation.js
@@ -1,5 +1,7 @@
 class Validation {
-  static NameRegex = /^[A-Za-z\'\s\.\:\-]+$/;
+  // static NameRegex = /^[A-Za-z\'\s\.\:\-]+$/;
+  static NameRegex =
+    /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/;
 }
 
 export default Validation;
