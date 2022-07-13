c5dac704477f7518abf181fa32a95d416464c28a
Carl Caraan <bannedefused@gmail.com>
2022-07-13 22:27:15 +0800

    7/13/22

--- frontend-react/src/pages/ProductCategoryPage.jsx
+++ frontend-react/src/pages/ProductCategoryPage.jsx
@@ -3,6 +3,8 @@ import FooterDesktop from "../components/common/FooterDesktop";
 import NavMenuDesktop from "../components/common/NavMenuDesktop";
 import Category from "../components/ProductDetails/Category";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import AppURL from "../../api/AppURL";
 
 export function withRouter(Children) {
   return (props) => {
@@ -21,14 +23,25 @@ class ProductCategoryPage extends Component {
   }
   componentDidMount() {
     window.scroll(0, 0);
-    alert(this.state.Category);
+    // alert(this.state.Category);
+    axios
+      .get(AppURL.ProductListByCategory(this.state.Category))
+      .then((response) => {
+        this.setState({
+          ProductData: response.data,
+        });
+      })
+      .catch((error) => {});
   }
 
   render() {
     return (
       <Fragment>
         <NavMenuDesktop />
-        <Category />
+        <Category
+          Category={this.state.Category}
+          ProductData={this.state.ProductData}
+        />
         <FooterDesktop />
       </Fragment>
     );
