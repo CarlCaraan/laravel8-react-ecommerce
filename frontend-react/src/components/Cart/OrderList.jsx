454997286b3521138b20f4831dac6df1a8a02e08
Carl Caraan <bannedefused@gmail.com>
2022-08-09 22:11:16 +0800

    8/9/22

--- frontend-react/src/components/Cart/OrderList.jsx
+++ frontend-react/src/components/Cart/OrderList.jsx
@@ -9,6 +9,7 @@ import {
   Form,
   Button,
   Card,
+  Modal,
 } from "react-bootstrap";
 import classes from "./OrderList.module.css";
 
@@ -19,6 +20,13 @@ class OrderList extends Component {
       ProductData: [],
       isLoading: "",
       mainDiv: "d-none",
+
+      // Modal
+      show: false,
+      NotificationData: [],
+      notificationTitle: "",
+      notificationMessage: "",
+      notificationDate: "",
     };
   }
 
@@ -35,6 +43,21 @@ class OrderList extends Component {
       })
       .catch((error) => {});
   }
+
+  handleClose = () => {
+    this.setState({ show: false });
+  };
+  handleShow = (event) => {
+    this.setState({ show: true });
+    let nTitle = event.target.getAttribute("data-title");
+    let nMessage = event.target.getAttribute("data-message");
+    let nDate = event.target.getAttribute("data-date");
+    this.setState({
+      notificationTitle: nTitle,
+      notificationMessage: nMessage,
+      notificationDate: nDate,
+    });
+  };
 
   render() {
     const OrderLists = this.state.ProductData;
@@ -89,13 +112,7 @@ class OrderList extends Component {
               </Col>
               <Col>
                 <Button
-                  onClick={() =>
-                    this.ItemMinus(
-                      OrderList.id,
-                      OrderList.quantity,
-                      OrderList.unit_price
-                    )
-                  }
+                  onClick={this.handleShow}
                   className={`${classes["custom-review-btn"]} mt-2`}
                 >
                   Write a Review
@@ -179,6 +196,33 @@ class OrderList extends Component {
           </Container>
         </div>
         {/* End Cart Section */}
+
+        {/* Start Modal */}
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <h6 className={`${classes["modal-header"]}`}>
+              Write a Review
+              {this.state.notificationDate}
+            </h6>
+          </Modal.Header>
+          <Modal.Body>
+            <h6 className={`${classes["modal-title"]}`}>
+              {this.state.notificationTitle}
+            </h6>
+            <p className={`${classes["modal-body"]}`}>
+              {this.state.notificationMessage}
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              className={`${classes["modal-button"]}`}
+              onClick={this.handleClose}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {/* End Modal */}
       </Fragment>
     );
   }
