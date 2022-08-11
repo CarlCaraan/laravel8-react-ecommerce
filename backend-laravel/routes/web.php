9e9a4ab4504caf1cfa30a12f07e713d8c37de322
Carl Caraan <bannedefused@gmail.com>
2022-08-11 22:18:04 +0800

    8/11/22

--- backend-laravel/routes/web.php
+++ backend-laravel/routes/web.php
@@ -12,6 +12,6 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.index');
     })->name('dashboard');
 });
